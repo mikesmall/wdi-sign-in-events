96a775d8cfd8ba776737982dfc3a17b54bca3352
mikesmall <michael.l.a.small@gmail.com>
2017-07-06 15:43:43 -0400

    Added crucially-missing <script src=js/main.js></script> to index.html

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 
-var modal = document.querySelector('.modal');
-var signIn = document.querySelector('.signin');
-var close = document.querySelector('.close');
-var submit = document.querySelector('.submit');
-// For later:
-// var error = document.querySelector('');
-//-------------------------------------------------------------------------------------
-// When the user presses the .signin button, fade in the modal window
-function showModalWindow(click) {
-  modal.style.display = 'block';
-  console.log('click received');
-};
+  var modal = document.querySelector('.modal');
+  var signIn = document.querySelector('.signin');
+  var close = document.querySelector('.close');
+  var submit = document.querySelector('.submit');
+  // For later:
+  // var error = document.querySelector('');
+  //-------------------------------------------------------------------------------------
+  // When the user presses the .signin button, fade in the modal window
+  function showModalWindow(click) {
+    modal.style.display = 'block';
+    console.log('click received');
+  };
 
-signIn.addEventListener('click', showModalWindow);
-//-------------------------------------------------------------------------------------
-// When the user presses the .close button, fade out the modal window
-function hideModalWindow(click) {
-  modal.style.display = 'none';
-};
+  signIn.addEventListener('click', showModalWindow);
+  //-------------------------------------------------------------------------------------
+  // When the user presses the .close button, fade out the modal window
+  function hideModalWindow(click) {
+    modal.style.display = 'none';
+  };
 
-close.addEventListener('click', hideModalWindow);
-//-------------------------------------------------------------------------------------
-// When the user presses the .submit button, add an .error class to both input elements
+  close.addEventListener('click', hideModalWindow);
+  //-------------------------------------------------------------------------------------
+  // When the user presses the .submit button, add an .error class to both input elements
 
 
-//-------------------------------------------------------------------------------------
-// When the user puts their cursor in one of the input fields, remove the .error class
+  //-------------------------------------------------------------------------------------
+  // When the user puts their cursor in one of the input fields, remove the .error class
 
-};
+});
