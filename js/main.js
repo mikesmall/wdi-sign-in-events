ee650290d4866564aa22218a7ae62fa5e8a47817
mikesmall <michael.l.a.small@gmail.com>
2017-07-06 16:00:26 -0400

    First try at 'input.error' querySelector

--- js/main.js
+++ js/main.js
@@ -4,28 +4,39 @@ document.addEventListener('DOMContentLoaded', function(event) {
   var signIn = document.querySelector('.signin');
   var close = document.querySelector('.close');
   var submit = document.querySelector('.submit');
-  // For later:
-  // var error = document.querySelector('');
+  var error = document.querySelector('input.error');
+
   //-------------------------------------------------------------------------------------
+
   // When the user presses the .signin button, fade in the modal window
+
   function showModalWindow(click) {
     modal.style.display = 'block';
-    console.log('click received');
   };
 
   signIn.addEventListener('click', showModalWindow);
+
   //-------------------------------------------------------------------------------------
+
   // When the user presses the .close button, fade out the modal window
+
   function hideModalWindow(click) {
     modal.style.display = 'none';
   };
 
   close.addEventListener('click', hideModalWindow);
+
   //-------------------------------------------------------------------------------------
+
   // When the user presses the .submit button, add an .error class to both input elements
 
+  
 
   //-------------------------------------------------------------------------------------
+
   // When the user puts their cursor in one of the input fields, remove the .error class
 
+
+
+  //-------------------------------------------------------------------------------------
 });
