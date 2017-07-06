d43bf4a51527b6ae20adbd5ced4a1d30c886e679
mikesmall <michael.l.a.small@gmail.com>
2017-07-06 15:35:17 -0400

    Functions added for displaying/hiding the Modal window. Not working yet.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
-// When the user presses the .signin button, fade in the modal window
-
-// When the user presses the .close button, fade out the modal window
-
-// When the user presses the .submit button, add an .error class to both input elements
-
-// When the user puts their cursor in one of the input fields, remove the .error class
-
-
 var modal = document.querySelector('.modal');
 var signIn = document.querySelector('.signin');
 var close = document.querySelector('.close');
 var submit = document.querySelector('.submit');
+// For later:
 // var error = document.querySelector('');
+//-------------------------------------------------------------------------------------
+// When the user presses the .signin button, fade in the modal window
+function displayModalWindow(click) {
+  modal.style.display = 'block';
+};
+
+signIn.addEventListener('click', displayModalWindow);
+//-------------------------------------------------------------------------------------
+// When the user presses the .close button, fade out the modal window
+function hideModalWindow(click) {
+  modal.style.display = 'none';
+};
+
+close.addEventListener('click', hideModalWindow);
+//-------------------------------------------------------------------------------------
+// When the user presses the .submit button, add an .error class to both input elements
+
+
+//-------------------------------------------------------------------------------------
+// When the user puts their cursor in one of the input fields, remove the .error class
